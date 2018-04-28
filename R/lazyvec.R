1df4c2ee4f5f92aa1991838e7f961d32c942ed9e
MarkKlik <markklik@gmail.com>
2018-04-28 12:10:04 +0200

    Start package lazyvec

---
+++ R/lazyvec.R
@@ -0,0 +1,12 @@
+
+# LICENSE lazyvec
+
+#' Create a lazy vector
+#'
+#' @param lazyvec_interface Interface class for the lazy vector
+#'
+#' @return Wrapper around a lazy vector
+#' @export
+lazyvec <- function(lazyvec_interface) {
+  
+}
