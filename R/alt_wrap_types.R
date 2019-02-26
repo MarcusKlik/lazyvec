79c29191b20e7bcb164864c2f05c99f0bb755b08
markklik <markklik@gmail.com>
2019-02-26 21:47:31 +0100

    Add license disclaimer to top of code files

--- R/alt_wrap_types.R
+++ R/alt_wrap_types.R
@@ -1,3 +1,24 @@
+#  lazyvec - R package for creating, testing and deploying custom ALTREP vectors
+#
+#  Copyright (C) 2019-present, Mark AJ Klik
+#
+#  This file is part of the lazyvec R package.
+#
+#  The lazyvec R package is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU Affero General Public License version 3 as
+#  published by the Free Software Foundation.
+#
+#  The lazyvec R package is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+#  for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License along
+#  with the lazyvec R package. If not, see <http://www.gnu.org/licenses/>.
+#
+#  You can contact the author at:
+#  - lazyvec R package source repository : https://github.com/fstpackage/lazyvec
+
 
 listeners <- function() {
 
